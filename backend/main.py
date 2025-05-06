36866c17788f6a4058fcfbfbfde2fcbca3c1c19d
smercas <125765610+smercas@users.noreply.github.com>
2025-05-06 19:00:08 +0000

    a whole bunch of nothing

--- backend/main.py
+++ backend/main.py
@@ -13,37 +13,6 @@ co = cohere.Client("8Fn0yGZ37F28qQpEHzvZuDU3ZQhNlXOVNEEE9VHP")
 
 CORS(app)
 
-questions = []
-for _, question_pack in questions_by_subscale.items():
-    questions += question_pack
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"   
-
-
-@app.route('/start')
-def start():
-    gender = Gender.M
-    
-    start_questions = [e.json for e in [
-        questions_by_subscale['Tulburare depresivă majoră'][0],
-        questions_by_subscale['Tulburare de stres posttraumatic'][4],
-        questions_by_subscale['Bulimie/Alimentație compulsivă'][1],
-        questions_by_subscale['Tulburare obsesiv-compulsivă'][2],
-        questions_by_subscale['Tulburare de panică'][3],
-        questions_by_subscale['Tulburări psihotice'][0],
-        questions_by_subscale['Agorafobie'][0],
-        questions_by_subscale['Fobie socială'][3],
-        questions_by_subscale['Abuz/dependență de alcool'][4],
-        questions_by_subscale['Abuz/dependență de medicamente'][4],
-        questions_by_subscale['Tulburare de anxietate generalizată'][3],
-        questions_by_subscale['Tulburare de somatizare'][0],
-        questions_by_subscale['Ipohondrie'][3],
-    ]]
-
-    return start_questions
 
 
 @app.route('/determine-forms', methods=['POST'])
@@ -56,6 +25,8 @@ def determine_forms():
 
     #1. extracting data
     data = request.form.get('text_data')
+    print(request.form)
+    print(data)
 
     #2. similarity check with the data
 
@@ -601,4 +572,4 @@ def fetch_diagnosis():
 
     pass
 
-print(select_next_subscale("sunt foarte fericit", {}))
+# print(select_next_subscale("sunt foarte fericit", {}))
